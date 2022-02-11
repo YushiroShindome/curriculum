04c34d8b596091d9f7a11a4c2d1d692d1c172ac1
YUSHIRO-S <78323542+YUSHIRO-S@users.noreply.github.com>
2022-02-11 16:59:51 +0900

    2章　チェックテスト

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,25 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+	
+	private static String firstName = "友志郎";
+	private static String lastName = "新留";
+	
+	private static void printName(String firstName, String lastName) {
+		String fullName = lastName + firstName;
+		System.out.println("printNameメソッド → " + fullName);
+	}
+	
+	public static void main(String[] args) {
+		printName(firstName, lastName);
+		
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		robotPet.introduce();
+	}
+}
+ 
